dc9902453b95570483aa308d2ef8a48d8e57da15
Priyank Patel <priyank.patel@uoit.net>
2016-03-07 18:30:05 -0500

    Added Comments

--- Phase3/BankingSystem/BankingSystem.cpp
+++ Phase3/BankingSystem/BankingSystem.cpp
@@ -1,11 +1,9 @@
-// BankingSystem.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include "TransactionsHelper.h"
 #include "FileStreamHelper.h"
 #include <iostream>
 
+// Define a new enumerated type
 typedef enum {
 	tLogin,
 	tLogout,
@@ -21,12 +19,24 @@ typedef enum {
 	unknown
 } trans_enum;
 
+/*
+ * @method trim
+ * @desc trim the whitespace from the inputs
+ * @params <std::string str> string with whitespace
+ * @return <std::string str> string with whitespace removed
+ */
 std::string trim(std::string str) {
     size_t first = str.find_first_not_of(' ');
     size_t last = str.find_last_not_of(' ');
     return str.substr(first, (last-first+1));
 }
 
+/*
+ * @method hascode
+ * @desc check the transaction codes
+ * @params <std::string cost& job> transaction code 
+ * @return <enum transaction> if the transaction matches
+ */
 trans_enum hascode(std::string const& job) {
 	if(trim(job) == "login") return tLogin;
 	if(trim(job) == "logout") return tLogout;
@@ -55,6 +65,8 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	std::string transactions;
+
+	// Switch through the input of transaction commands and execute its process
 		while(std::cin >> transactions) {
 			switch(hascode(trim(transactions))) {
 			case tLogin:
