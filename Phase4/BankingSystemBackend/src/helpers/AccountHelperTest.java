891b1ab1e0f95c611d73fa0d0f06addd27f31ce0
PR1YANKPAT3L <silentcoder@live.ca>
2016-03-23 18:32:37 -0400

    Removed Unecessary Tests

--- Phase4/BankingSystemBackend/src/helpers/AccountHelperTest.java
+++ Phase4/BankingSystemBackend/src/helpers/AccountHelperTest.java
@@ -357,66 +357,6 @@ public class AccountHelperTest {
 	@Test
 	public final void testGetAccountList() {
 		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testObject() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetClass() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testHashCode() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testEquals() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testClone() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testToString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testNotify() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testNotifyAll() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testWaitLong() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testWaitLongInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testWait() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testFinalize() {
-		fail("Not yet implemented"); // TODO
 	}*/
 
 }
