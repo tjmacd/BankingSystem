0ac5ff26a7c0c4e1e9fb7ab8a07a82d17c395ead
Timothy MacDougall <timothy.macdougall@uoit.net>
2016-03-23 18:05:16 -0400

    Test cases for readMergedTransFile()

--- Phase4/BankingSystemBackend/src/helpers/FileStreamHelperTest.java
+++ Phase4/BankingSystemBackend/src/helpers/FileStreamHelperTest.java
@@ -2,15 +2,72 @@ package helpers;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FileStreamHelperTest {
-
-	@Test
-	public void testReadMergedTransFile() {
-		fail("Not yet implemented");
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
 	}
 
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	    System.setErr(null);
+	}
+
+	@Test // Case 1: transaction code 2 - execute lines 73-77
+	public void testReadMergedTransFile1() {
+		FileStreamHelper fsh = new FileStreamHelper();
+		fsh.setMerged_transaction_file("transferTransaction.txt");
+		ArrayList<Transactions> list = fsh.readMergedTransFile();
+		assertEquals(1, list.size());
+		
+		assertEquals("00002", list.get(0).misc);
+	}
+
+	@Test // Case 2: transaction code not 2 - execute line 80
+	public void testReadMergedTransFile2() {
+		FileStreamHelper fsh = new FileStreamHelper();
+		fsh.setMerged_transaction_file("paybillTransaction.txt");
+		ArrayList<Transactions> list = fsh.readMergedTransFile();
+		assertEquals(1, list.size());
+		
+		assertEquals("TV", list.get(0).misc);
+	}
+	
+	@Test // Case 3: file not found - line 87-89
+	public void testReadMergedTransFile3() {
+		FileStreamHelper fsh = new FileStreamHelper();
+		fsh.setMerged_transaction_file("nofile");
+		ArrayList<Transactions> list = fsh.readMergedTransFile();
+		
+		String exception = errContent.toString().split(" ")[0];
+		assertEquals("java.io.FileNotFoundException:", exception);
+	}
+	
+	@Test // Case 4: io exception - lines 90-92
+	public void testReadMergedTransFile4() {
+		FileStreamHelper fsh = new FileStreamHelper();
+		fsh.setMerged_transaction_file("folder");
+		ArrayList<Transactions> list = fsh.readMergedTransFile();
+		
+		String exception = errContent.toString().split(" ")[0];
+		
+		assertEquals("java.io.IOException:", exception);
+	}
+	
 	@Test
 	public void testReadOldAccFile() {
 		fail("Not yet implemented");
