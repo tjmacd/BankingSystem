9b953d1fea68e2f7f110754ae7c76472c39e3d65
PR1YANKPAT3L <silentcoder@live.ca>
2016-03-23 17:59:38 -0400

    Add testChangePlan Test

--- Phase4/BankingSystemBackend/src/helpers/AccountHelperTest.java
+++ Phase4/BankingSystemBackend/src/helpers/AccountHelperTest.java
@@ -209,12 +209,39 @@ public class AccountHelperTest {
 		assertEquals(null, ah1.getAccount(2));  // Check if it returns null
 	}
 
-	/*@Test
+	@Test
 	public final void testChangePlan() {
-		fail("Not yet implemented"); // TODO
+		ArrayList<Transactions> trans = new ArrayList<Transactions>();
+		ArrayList<Accounts> accs = new ArrayList<Accounts>();
+		
+		Accounts a = new Accounts();
+		a.number = 1;
+		a.name = "Account 1";
+		a.is_active = true;
+		a.balance = 1000;
+		a.trans_count = 0;
+		a.is_student = false;
+		accs.add(a);
+		
+		Transactions t = new Transactions();
+		t.code = 8;
+		t.name = "Account 1";
+		t.number = 1;
+		t.amount = 100;
+		t.misc = "1";
+		trans.add(t);
+		
+		AccountsHelper ah1 = new AccountsHelper(trans, accs);
+		
+		// Test to change the plan of the non-student to student
+		assertNotEquals(true, ah1.getAccount(a.number).is_student); // Check if the account plan is non-student
+		ah1.changePlan(a.name, a.number);
+		assertEquals(true, ah1.getAccount(a.number).is_student);  // Check if it the account plan is student
+		ah1.changePlan(a.name, a.number);
+		assertEquals(false, ah1.getAccount(a.number).is_student);  // Check if it the account plan is non-student
 	}
 
-	@Test
+	/*@Test
 	public final void testChangeStatus() {
 		fail("Not yet implemented"); // TODO
 	}
